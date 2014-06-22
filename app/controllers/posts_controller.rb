8437b3d02919f5f15da80054c0d7ccbf5211b870
Justin Veirs <justinveirs@gmail.com>
2014-06-21 19:01:11 -0600

    static controller with home about config the route to reflect new paths

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,5 +12,20 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      redirect_to posts_path
+
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
+  root 'static#home'
+  get '/about', to: 'static#about', as: :about
 end
